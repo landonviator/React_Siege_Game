915fbcd7168262bc6297991fcd84802c780e03fc
Landon Viator <landonviator@gmail.com>
2020-04-30 17:24:26 -0500

    Hosted on netlify

---
+++ build/precache-manifest.239b2fb3646af4d6aaff92467ad10eec.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d7f03f379ffd0d9accc26c8b6030f4a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c328f8531586c9d68b6a",
+    "url": "/static/css/main.8afa1fbd.chunk.css"
+  },
+  {
+    "revision": "80196377a25f65b04528",
+    "url": "/static/js/2.06e82339.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.06e82339.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c328f8531586c9d68b6a",
+    "url": "/static/js/main.8a49ac13.chunk.js"
+  },
+  {
+    "revision": "558ec6951597ddf5ae7b",
+    "url": "/static/js/runtime-main.355b39b7.js"
+  },
+  {
+    "revision": "2e81df3bde53d45ccf4e48662b7549a3",
+    "url": "/static/media/LittleMonster-01.2e81df3b.png"
+  }
+]);
